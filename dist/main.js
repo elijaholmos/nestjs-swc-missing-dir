c22cf7272e18fe0478994864754bd9f724b00967
elijaholmos <eli.olmos9@gmail.com>
2023-06-26 11:55:47 -0700

    chore: add `dist` to gitignore

--- dist/main.js
+++
@@ -1,7 +0,0 @@
-// This entire file is executed in a node environment by Nest.js when `nest start` is run.
-"use strict";
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
-const _app = require("./app");
-(0, _app.initializeApp)();
